ed705dd5e9d609c8532dd0dd50c416cfef2443d7
bdustin55 <158865280+bdustin55@users.noreply.github.com>
2024-04-21 20:48:33 -0400

    Updated profile.model.js

--- models/profile.model.js
+++ models/profile.model.js
@@ -3,6 +3,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const profileSchema = new Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+    unique: true
+  },
   age: {
     type: Number,
     required: true,
